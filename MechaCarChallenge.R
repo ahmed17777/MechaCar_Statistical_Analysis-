bcfc81e707978274c50922416fb7ecc79d9aed1e
ahmed17777 <mishaal22s@gmail.com>
2021-06-07 19:41:17 -0400

    Deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -12,3 +12,13 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 #Determine summary statistics 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar))
 
+#DELIVERABLE 2
+
+#Read csv
+SpnCoil <- read.csv('Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+
+#Create PSI summary 
+total_summary <- SpnCoil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+#Create lot summary 
+lot_summary <- SpnCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
